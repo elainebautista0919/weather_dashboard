f813b9eb2be9574f564a850ee6ddb852e010a968
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-11-13 13:13:07 -0500

    Added fucntion for current and forecast weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,60 @@ function findCity() {
     })
 }
 
+// Function for cities already in the list
+function getListCity(coordinates) {
+    apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                getCurrentWeather(data);
+            })
+        }
+    })
+}
+
+// Function to get current weather
+function getCurrentWeather(data) {
+    $(".results-panel").addClass("visible");
+
+    $("#currentIcon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
+    $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
+    $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
+    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
+    $("#uv-index")[0].textContent = "  " + data.current.uvi;
+
+    if (data.current.uvi < 3) {
+        $("#uv-index").removeClass("moderate severe");
+        $("#uv-index").addClass("favorable");
+    } else if (data.current.uvi < 6) {
+        $("#uv-index").removeClass("favorable severe");
+        $("#uv-index").addClass("moderate");
+    } else {
+        $("#uv-index").removeClass("favorable moderate");
+        $("#uv-index").addClass("severe");
+    }
+
+    getFutureWeather(data);
+}
+
+// Function to get forecast weather
+function getFutureWeather(data) {
+    for (var i = 0; i < 5; i++) {
+        var futureWeather = {
+            date: convertUnixTime(data, i),
+            icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
+            temp: data.daily[i + 1].temp.day.toFixed(1),
+            humidity: data.daily[i + 1].humidity
+        }
+
+        var currentSelector = "#day-" + i;
+        $(currentSelector)[0].textContent = futureWeather.date;
+        currentSelector = "#img-" + i;
+        $(currentSelector)[0].src = futureWeather.icon;
+        currentSelector = "#temp-" + i;
+        $(currentSelector)[0].textContent = "Temp: " + futureWeather.temp + " \u2109";
+        currentSelector = "#hum-" + i;
+        $(currentSelector)[0].textContent = "Humidity: " + futureWeather.humidity + "%";
+    }
+}
